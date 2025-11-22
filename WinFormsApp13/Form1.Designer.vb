b0eb0a4b22695d875c487ceb3e0344f4b37bc0b4
ioslm771-png <ioslm771@gmail.com>
2025-11-22 21:17:04 +0300

    Add project files.

---
+++ WinFormsApp13/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(474, 229)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "substring "
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(323, 229)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "remove "
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(171, 229)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 2
+        Button3.Text = "insert"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(171, 56)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(249, 23)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(171, 89)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(249, 23)
+        TextBox2.TabIndex = 4
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(171, 122)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(249, 23)
+        TextBox3.TabIndex = 5
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(171, 155)
+        TextBox4.Multiline = True
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(249, 23)
+        TextBox4.TabIndex = 6
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(488, 59)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(70, 15)
+        Label1.TabIndex = 7
+        Label1.Text = "اسم الشخص"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(497, 90)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(54, 15)
+        Label2.TabIndex = 8
+        Label2.Text = "اسم الاب "
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(497, 122)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(50, 15)
+        Label3.TabIndex = 9
+        Label3.Text = "اسم الجد"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(508, 155)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(39, 15)
+        Label4.TabIndex = 10
+        Label4.Text = "النتيجه"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+
+End Class
---
+++ WinFormsApp13/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim fullname As String = "زايد ناصر طالب "
+        Dim father As String = fullname.Substring(5, 4)
+        TextBox4.Text = father
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim fullname As String = "زايد ناصر طالب "
+        Dim cleanstr As String = fullname.Remove(5, 5)
+        TextBox4.Text = cleanstr
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim fullname As String = "زايد ناصر طالب "
+        Dim newstr As String = fullname.Insert(0, "ناصر ")
+        TextBox4.Text = newstr
+    End Sub
+End Class
